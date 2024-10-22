778e9208f431b57600fe6e3a3b5510a9f41420d9
greeshmasaiitla <156201372+greeshmasaiitla@users.noreply.github.com>
2024-10-22 13:38:07 +0530

    Update dev.java

--- dev.java
+++ dev.java
@@ -1,7 +1,7 @@
-class dev
+public class dev
 {
-public static void main(String arg[])
+public static void main(String[] args)
 {
-System.out.println("hello world")
+System.out.println("hello world");
 }
 }
